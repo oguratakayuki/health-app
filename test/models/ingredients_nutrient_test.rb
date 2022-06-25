4c6bf2ac11a4725ed013a762b3d20d1f520737ad
oguratakayuki <otn.ogura@gmail.com>
2022-06-25 16:47:39 +0900

    initial commit

---
+++ test/models/ingredients_nutrient_test.rb
@@ -0,0 +1,7 @@
+require "test_helper"
+
+class IngredientsNutrientTest < ActiveSupport::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
---
+++ test/models/nutrients_intake_standard_test.rb
@@ -0,0 +1,7 @@
+require "test_helper"
+
+class NutrientsIntakeStandardTest < ActiveSupport::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
